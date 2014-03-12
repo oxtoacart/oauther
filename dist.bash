5f3098a86112d24c8e8c526e2cab883977e5ce8c
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-12 06:43:46 -0500

    Script comment update

--- dist.bash
+++ dist.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # This script builds binaries for windows, linux and mac, compresses them with
-# upx and puts them in the dist folder.
+# upx and puts them in the dist folder.  It requires that you have the golang
+# cross-compilation tools installed at ~/git/golang-crosscompile as described
+# by this article:
+# http://dave.cheney.net/2012/09/08/an-introduction-to-cross-compilation-with-go
+
+
 source ~/git/golang-crosscompile/crosscompile.bash
 mkdir dist
 go-windows-386 build -o dist/oauther_i386.exe
